e1753a088924ab872758f486e9ca25b5c2768096
Sergey Sizow <sergey_sizow@ukr.net>
2017-03-18 20:07:01 +0200

    add pagination

--- students/views/students.py
+++ students/views/students.py
@@ -4,20 +4,39 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 from ..models import Student
+
 
 # Views for Students
 
 def students_list(request):
     students = Student.objects.all()
 
-    #try to order students list
+    # try to order students list
 
     order_by = request.GET.get('order_by', '')
     if order_by in ('last_name', 'first_name', 'ticket'):
         students = students.order_by(order_by)
         if request.GET.get('reverse', '') == '1':
             students = students.reverse()
+
+    else:
+        students = students.order_by('last_name')
+
+    # paginator students
+
+    paginator = Paginator(students, 3)
+    page = request.GET.get('page')
+    try:
+        students = paginator.page(page)
+    except PageNotAnInteger:
+        # If page not integer, deliver first page
+        students = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range, deliver last page of results
+        students = paginator.page(paginator.num_pages)
 
     return render(request, 'students/students_list.html', {'students': students})
 
@@ -32,6 +51,3 @@ def students_edit(request, sid):  # sid= student id
 
 def students_delete(request, sid):
     return HttpResponse('<h1>Delete Student %s</h1>' % sid)
-
-
- 
