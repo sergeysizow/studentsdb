b724a20293913c7befd03d7cd850b69c39822e38
Sergey Sizow <sergey_sizow@ukr.net>
2017-05-02 20:48:46 +0300

    add exams and pagination. Changed db

---
+++ students/models/journal.py
@@ -0,0 +1,42 @@
+# _*_ coding: utf-8 _*_
+
+from __future__ import unicode_literals
+
+from django.db import models
+
+import calendar
+
+v = calendar.Calendar(firstweekday=0)
+
+
+class Journal(models.Model):
+    """ Students model """
+
+    # user-frendly name in admin
+
+    class Meta(object):
+        verbose_name = u"Журнал",
+        verbose_name_plural = u"Журнали"
+
+    name = models.ForeignKey(
+        'Student',
+        verbose_name=u"Студент",
+        blank=False,
+        null=False,
+    )
+
+    group = models.OneToOneField(
+        'Group',
+        verbose_name=u"Група",
+        blank=False,
+        null=False,
+    )
+
+    for date in v.itermonthdays(2017, 4):
+        date = models.NullBooleanField(
+            verbose_name=u"дата",
+            default=True)
+
+    # chandge name in admin for human))
+    def __unicode__(self):
+        return u"%s %s" % (self.name, self.group)
