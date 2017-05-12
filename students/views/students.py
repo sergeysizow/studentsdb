e5a54d137e5d3836135fc11500fe4f15d3b6fc65
Sergey Sizow <sergey_sizow@ukr.net>
2017-05-12 20:15:07 +0300

    add form for student for photo

--- students/views/students.py
+++ students/views/students.py
@@ -8,6 +8,10 @@ from django.core.urlresolvers import reverse
 
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
+from datetime import datetime
+
+from PIL import Image
+
 from ..models.students import Student
 
 from ..models.groups import Group
@@ -47,27 +51,72 @@ def students_list(request):
 
 
 def students_add(request):
-    #was form posted?
+    # was form posted?
     if request.method == "POST":
         # was form are button clicked?
         if request.POST.get('add_button') is not None:
 
-            #TODO: validate input form user
+            # errors collection
             errors = {}
 
-            if not errors:
-                # create student object
-                student = Student(
-                    first_name=request.POST['first_name'],
-                    last_name=request.POST['last_name'],
-                    middle_name=request.POST['middle_name'],
-                    birthday=request.POST['birthday'],
-                    ticket=request.POST['ticket'],
-                    student_group=Group.objects.get(pk=request.POST['student_group']),
-                    photo=request.FILES['photo'],
-                )
+            # validate student data will go here
+            data = {'middle_name': request.POST.get('middle_name'), 'notes': request.POST.get('notes')}
 
-                # save student to database
+            # validate user input
+            first_name = request.POST.get('first_name', '').strip()
+            if not first_name:
+                errors['first_name'] = u"Ім'я є обов'язковим для заповнення"
+            else:
+                data['first_name'] = first_name
+
+            last_name = request.POST.get('last_name', '').strip()
+            if not last_name:
+                errors['last_name'] = u"Прізвище є обов'язковим для заповнення"
+            else:
+                data['last_name'] = last_name
+
+            birthday = request.POST.get('birthday', '').strip()
+            if not birthday:
+                errors['birthday'] = u"Дата народження є обов'язковою"
+            else:
+                data['birthday'] = birthday
+
+            ticket = request.POST.get('ticket', '').strip()
+            if not ticket:
+                errors['ticket'] = u"Квиток є обов'язковим"
+            else:
+                try:
+                    datetime.strptime(birthday, '%Y-%m-%d')
+                except Exception:
+                    errors['birthday'] = u"Ведіть коректний формат дати (наприклад 1989-08-24)"
+                else:
+                    data['ticket'] = ticket
+
+            student_group = request.POST.get('student_group', '').strip()
+            if not student_group:
+                errors['student_group'] = u"Оберіть групу для студента"
+            else:
+                groups = Group.objects.filter(pk=student_group)
+                if len(groups) !=1:
+                    errors['student_group'] = u"Оберіть коректну групу"
+                else:
+                    data['student_group'] = groups[0]
+
+            photo = request.FILES.get('photo')
+            list = ('BMP', 'EPS', 'GIF', 'JPEG', 'PDF', 'PNG', 'PNM', 'TIFF')
+            img = Image.open(photo, 'r')
+            format = img.format
+
+            
+
+            if format in list:
+                data['photo'] = photo
+            else:
+                errors['photo'] = u"Завантажте фото"
+
+            # save student to database
+            if not errors:
+                student = Student(**data)
                 student.save()
 
                 # redirect user to students_list
---
+++ studentsdb/db.py
@@ -0,0 +1,12 @@
+# Database
+# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'localhost',
+        'USER': 'students_db_user',
+        'PASSWORD': 'aezakmi_5090608_AEZAKMI',
+        'NAME': 'students_db',
+    }
+}
